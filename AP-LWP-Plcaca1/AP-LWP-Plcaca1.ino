6754867b7cbdaa2d48b96ea1d5914073a9b79c9b
luc4as-calixto <lucas_calixto-souza@estudante.sesisenai.org.br>
2025-11-05 16:44:15 -0300

    firts commit

---
+++ AP-LWP-Plcaca1/AP-LWP-Plcaca1.ino
@@ -0,0 +1,11 @@
+// placa1 para sensoriamento (ocupação, temperatura e umidade);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
